2da05d8df0d9924b3e79b11e98f799fc6805f2df
Cubby DeBry <cubbydebry@gmail.com>
2025-10-28 14:59:59 -0600

    moved bulk of work into run_test, added priority inversion test (still needs pass/fail conditions)

--- test/test_signaling.c
+++ test/test_signaling.c
@@ -14,58 +14,61 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+void run_test(void *params){
+    TaskArgs *args = (TaskArgs *) params;
+
+    TaskHandle_t task_handle_1;
+    xTaskCreate(args->t1_fn, "task 1", configMINIMAL_STACK_SIZE, (void*)args, tskIDLE_PRIORITY+5, &task_handle_1);
+
+    TaskHandle_t task_handle_2;
+    xTaskCreate(args->t2_fn, "task 2", configMINIMAL_STACK_SIZE, (void*)args, tskIDLE_PRIORITY+1, &task_handle_2);
+
+    TaskHandle_t task_handle_3;
+    xTaskCreate(args->t3_fn, "task 3", configMINIMAL_STACK_SIZE, (void*)args, tskIDLE_PRIORITY+2, &task_handle_3);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    TaskStatus_t stat_1, stat_2, stat_3;
+    
+    vTaskGetInfo(task_handle_1, &stat_1, pdTRUE, eInvalid);
+    vTaskGetInfo(task_handle_2, &stat_2, pdTRUE, eInvalid);
+    vTaskGetInfo(task_handle_3, &stat_3, pdTRUE, eInvalid);
+
+    uint64_t t1 = 0, t2 = 0, t3 = 0;
+
+    t1 = ulTaskGetRunTimeCounter(task_handle_1);
+    t2 = ulTaskGetRunTimeCounter(task_handle_2);
+    t3 = ulTaskGetRunTimeCounter(task_handle_3);
+
+    printf("Task 1 time: %llu\n", t1);
+    printf("Task 2 time: %llu\n", t2);
+    printf("Task 3 time: %llu\n", t3);
+}
+
+void priority_inversion(void){
+    SemaphoreHandle_t sem;
+    sem = xSemaphoreCreateBinary();
+
+    TaskArgs args = {
+        .sem = sem,
+        .t1_fn = task1,
+        .t1_priority = tskIDLE_PRIORITY + 5,
+        .t2_fn = task2,
+        .t2_priority = tskIDLE_PRIORITY + 1,
+        .t3_fn = task3,
+        .t3_priority = tskIDLE_PRIORITY + 2
+    };
+
+    run_test((void*)&args);
+}
+
 void test_task(void *params) {
     while(1){
         UNITY_BEGIN();
         // Run Tests
 
         // Activity 0
-        // RUN_TEST();
-
-        SemaphoreHandle_t sem;
-        sem = xSemaphoreCreateBinary();
-
-        TaskArgs args = {
-            .sem = sem,
-            .t1_fn = task1,
-            .t1_priority = tskIDLE_PRIORITY + 5,
-            .t2_fn = task2,
-            .t2_priority = tskIDLE_PRIORITY + 1,
-            .t3_fn = task3,
-            .t3_priority = tskIDLE_PRIORITY + 2
-        };
-
-        TaskHandle_t task_handle_1;
-        xTaskCreate(args.t1_fn, "task 1", configMINIMAL_STACK_SIZE, (void*)&args, tskIDLE_PRIORITY+5, &task_handle_1);
-
-        TaskHandle_t task_handle_2;
-        xTaskCreate(args.t2_fn, "task 2", configMINIMAL_STACK_SIZE, (void*)&args, tskIDLE_PRIORITY+1, &task_handle_2);
-
-        TaskHandle_t task_handle_3;
-        xTaskCreate(args.t3_fn, "task 3", configMINIMAL_STACK_SIZE, (void*)&args, tskIDLE_PRIORITY+2, &task_handle_3);
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        TaskStatus_t stat_1, stat_2, stat_3;
-        
-        vTaskGetInfo(task_handle_1, &stat_1, pdTRUE, eInvalid);
-        vTaskGetInfo(task_handle_2, &stat_2, pdTRUE, eInvalid);
-        vTaskGetInfo(task_handle_3, &stat_3, pdTRUE, eInvalid);
-
-        uint64_t t1 = 0, t2 = 0, t3 = 0;
-
-        t1 = ulTaskGetRunTimeCounter(task_handle_1);
-        t2 = ulTaskGetRunTimeCounter(task_handle_2);
-        t3 = ulTaskGetRunTimeCounter(task_handle_3);
-
-        printf("Task 1 time: %llu\n", t1);
-        printf("Task 2 time: %llu\n", t2);
-        printf("Task 3 time: %llu\n", t3);
-        
-
-        
-        
-
+        RUN_TEST(priority_inversion);
 
         // Activity 1
 
